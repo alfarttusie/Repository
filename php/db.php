3793e143dbaa7646e552a293e48ccf2e7cbf687a
alfarttusie <m.alfarttusie@gmail.com>
2025-01-10 06:22:35 +0300

    delete db.php file

--- php/db.php
+++
@@ -1,14 +0,0 @@
-<?php
-        trait database{
-            	private static  $Serverip =  'repository.iq';
-
-            	private  static $ServerUser = 'root';
-
-            	private  static $ServerPassword = '';
-
-            	private  static $database = 'repository';
-
-            	private  static $secret = 'zfh7birrncrdffwawps88oml1mo90xxkbyffy0v1elgmwtqz2f6xlp83792fieyuwjhhy2zgwmiywxnbwvrlnrv6u1sqbkf1qebd8agsqsir4qi0wbykjjoael01zhogzth0j10fnynjiqftmp0h16bee680q1nerieq53c3qj3qfa6i3w9dr4o75ymoyt8cdllmrdfih4j1hlt9vwg6ktydfs62eqyk8np04g6gh707ye7zotmojyvyrw62n6tareg2ld1s4jcdahmm8z6omq4q0c2m8jhb2x6798vbeyqszhs0ksuyiazl';
-        	}
-        
-        
